dab297f58670de04e1cad68c8904237e56255ff5
saavedras22-sys <saavedras22@itisvinci.com>
2026-03-20 17:12:17 +0100

    fixed button at start

--- Assets/Scripts/SpawnInView.cs
+++ Assets/Scripts/SpawnInView.cs
@@ -25,6 +25,23 @@ public class SpawnInView : MonoBehaviour
         transform.Rotate(0, 180, 0); // LookAt points at the back of the menu
     }
 
+    void Start()
+    {
+        // Get the camera's position and forward direction
+        Transform camTransform = Camera.main.transform;
+        
+        // Calculate the target position in front of the camera (ignores Y)
+        Vector3 forwardFixed = camTransform.forward;
+        forwardFixed.y = 0; 
+        Vector3 targetPosition = camTransform.position + (forwardFixed.normalized * distance);
+
+        // Instant snap to view
+        transform.position = targetPosition;
+
+        transform.LookAt(camTransform);
+        transform.Rotate(0, 180, 0); // LookAt points at the back of the menu
+    }
+
     // Update is called once per frame
     void Update()
     {/*
