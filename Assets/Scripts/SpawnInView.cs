7a7fe7b9787d4145d63778860ffb0e5109abea64
saavedras22-sys <saavedras22@itisvinci.com>
2026-03-20 16:25:20 +0100

    hidden welcome

--- Assets/Scripts/SpawnInView.cs
+++ Assets/Scripts/SpawnInView.cs
@@ -2,8 +2,11 @@ using UnityEngine;
 
 public class SpawnInView : MonoBehaviour
 {
-    public float distance; // Distance from the camera
+    public float distance = 2.0f; // Distance from the camera
     private bool positioned = false;
+    /*private GameObject mainMenu;
+    private GameObject map;
+    private GameObject guide;*/
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Awake()
     {
@@ -42,5 +45,32 @@ public class SpawnInView : MonoBehaviour
             transform.Rotate(0, 180, 0); // LookAt points at the back of the menu
             positioned = true;
         }*/
+    }
+    public void position()
+    {/*
+        mainMenu = GameObject.Find("MainMenu");
+        mainMenu.transform.GetChild(0).gameObject.SetActive(true);
+        map = GameObject.Find("Menu_Map");
+        guide = GameObject.Find("Guide");
+
+        Transform camTransform = Camera.main.transform;
+        
+        // Calculate the target position in front of the camera (ignores Y)
+        Vector3 forwardFixed = camTransform.forward;
+        forwardFixed.y = 0; 
+        Vector3 targetPosition = camTransform.position + (forwardFixed.normalized * distance);
+
+        // Instant snap to view
+        mainMenu.transform.position = targetPosition;
+        mainMenu.transform.LookAt(camTransform);
+        mainMenu.transform.Rotate(0, 180, 0);
+
+        map.transform.position = targetPosition;
+        map.transform.LookAt(camTransform);
+        map.transform.Rotate(0, 180, 0);
+
+        guide.transform.position = targetPosition;
+        guide.transform.LookAt(camTransform);
+        guide.transform.Rotate(0, 180, 0);*/
     }
 }
