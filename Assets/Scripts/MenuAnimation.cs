57712eb25a7cf4e5f4b3db5b380e463b9bc83054
saavedras22-sys <saavedras22@itisvinci.com>
2026-03-24 11:49:54 +0100

    audio assets added

--- Assets/Scripts/MenuAnimation.cs
+++ Assets/Scripts/MenuAnimation.cs
@@ -2,6 +2,9 @@ using UnityEngine;
 
 public class MenuAnimation : MonoBehaviour
 {
+    public float scaleX;
+    public float scaleY;
+    public float scaleZ;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {/*
@@ -19,7 +22,7 @@ public class MenuAnimation : MonoBehaviour
     public void StartAnimation()
     {
         Debug.Log("playa animazione");
-        Vector3 originalScale = transform.localScale;
+        Vector3 originalScale = new Vector3(scaleX, scaleY, scaleZ);
         transform.localScale = Vector3.zero;
         transform.LeanScale(originalScale, 0.2f).setEaseInOutBack();
     }
